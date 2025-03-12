8e7e3c4e4bfb65ead1831867edd495ecc3e728b1
shoulansi <garrettswhite@gmail.com>
2025-03-12 08:27:25 -0500

    final character checker

--- characterchecker.js
+++ characterchecker.js
@@ -1,1 +1,6 @@
+const readline = require('readline-sync');
+let testApp = "This is a Test!"
+console.log(testApp)
 
+let quesOne = readline.question("What character is the 6th index point?")
+console.log(testApp[6] + " is the correct answer!")
